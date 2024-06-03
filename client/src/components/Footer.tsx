ba5725e2f2bf638ceec07fe1e815a72268d7601b
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-06-03 15:10:58 +0200

    added a footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import "../styles/footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="box1">
+        <p>box 1</p>
+      </div>
+      <div className="box2">
+        <p>box 2</p>
+      </div>
+      <div className="box3">
+        <p>box 3</p>
+      </div>
+    </div>
+  );
+};

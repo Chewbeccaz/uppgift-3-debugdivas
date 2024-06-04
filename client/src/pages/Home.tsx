5ccea279615b37b89fa6c0e65df0a45949de1e3e
NurRydberg <nur.rydberg@gmail.com>
2024-06-04 14:35:48 +0200

    fish swim random now

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,39 @@
-import { Login } from "../components/Login";
 import { motion } from "framer-motion";
 import fisk from "../assets/fisk.png";
 import fisk2 from "../assets/fisk2.png";
 import "../styles/home.css";
+import React, { useState, useEffect } from 'react'; // Importera useState och useEffect
 
 export const Home = () => {
+  // Skapar en state för att lagra den aktuella toppositionen för fisk1
+  const [topPosition, setTopPosition] = useState('50px'); // Startposition
+
+  // Funktion för att generera en slumpmässig topposition
+  const generateRandomTopPosition = () => {
+    const minTop = 10; // Minsta topposition i px
+    const maxTop = 1000; // Maximala topposition i px
+    const randomTop = Math.floor(Math.random() * (maxTop - minTop + 1)) + minTop;
+    return `${randomTop}px`;
+  };
+// Uppdaterar toppositionen när komponenten renderas
+useEffect(() => {
+  setTopPosition(generateRandomTopPosition());
+}, []);
+
+// Effekt för att uppdatera toppositionen efter varje passering
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setTopPosition(generateRandomTopPosition());
+  }, 20000); // Ställer in en timeout för att uppdatera positionen efter 20 sekunder
+
+  return () => clearTimeout(timer); // Rensa upp timern när komponenten destrueras
+}, [topPosition]); // Dependencyn är topPosition så att effekten körs igen när toppositionen ändras
+
   return (
     <>
       <h3>Välkommen till</h3>
       <h2>havsnyheter!</h2>
+
       <div>
         <motion.img
           src={fisk}
@@ -29,11 +54,12 @@ export const Home = () => {
             right: 0,
             width: "8%",
             height: "auto",
+            top: topPosition, // Använder state-värdet för toppositionen
           }}
         />
       </div>
 
-      <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+      <div style={{ position: "relative", width: "100%", height: "100vh", overflow:"hidden"}}>
         <motion.img
           src={fisk2}
           id="fisk2"

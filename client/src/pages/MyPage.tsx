781b4f335b50de47a023c537d7bb75c7f97be563
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-06-11 21:31:40 +0200

    Added some styling (wip) and information

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,11 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Login } from '../components/Login';
 import { useUser } from '../context/UserContext';
 import axios from 'axios';
+import "../styles/mypage.css";
 
+
+interface SubscriptionInfo {
+  subscriptionLevel: string;
+  lastPaymentDate: number;
+  nextPaymentDate: number;
+}
 export const MyPage = () => {
   const { user } = useUser();
   const [message, setMessage] = useState('');
+  const [subscriptionInfo, setSubscriptionInfo] = useState<SubscriptionInfo | null>(null);
+  
+
+  useEffect(() => {
+    if (user) {
+      const fetchSubscriptionInfo = async () => {
+        try {
+          const response = await axios.get('/api/stripe/subscription-info', {
+            params: { userId: user.userId },
+          });
+          setSubscriptionInfo(response.data);
+        } catch (error) {
+          console.error('Error fetching subscription info:', error);
+        }
+      };
+
+      fetchSubscriptionInfo();
+    }
+  }, [user]);
 
   const cancelSubscription = async () => {
     if (user) {
@@ -26,15 +52,29 @@ export const MyPage = () => {
   };
 
   return (
-    <div>
+    <div className="container-mypage"> 
+    <div className="box-mypage">
       {user ? (
         <>
-        <button onClick={cancelSubscription}>Cancel Subscription</button>
-          {message && <p>{message}</p>}
+        {subscriptionInfo ? ( 
+            <div>
+              <h4>Prenumerationsnivå: {subscriptionInfo.subscriptionLevel}</h4>             
+              <p>Senaste betalning: {new Date(subscriptionInfo.lastPaymentDate * 1000).toLocaleDateString()}</p>
+              <p>Nästa betalning: {new Date(subscriptionInfo.nextPaymentDate * 1000).toLocaleDateString()}</p>
+              {message && <p>{message}</p>}
+              <button>Uppgradera??</button>
+              <button className="cancel-btn" onClick={cancelSubscription}>Cancel Subscription</button>
+            </div>
+           ) : (
+            <div>
+              <p>Hämtar prenumerationsinformation</p>
+              <div className="loader-mypage"></div>
+            </div>
+          )}
         </>
       ) : (
         <Login />
-    )}
+      )}
     </div>
-  );
-};
+</div>
+  ) }
